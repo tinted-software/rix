75891212f5d51b14c21496fcb61f8a69a15554b9
Theo Paris <theo@theoparis.com>
2026-04-18 21:51:04 -0700

    refactor: clean up cli
    Signed-off-by: Theo Paris <theo@theoparis.com>

--- rix-cli/src/main.rs
+++ rix-cli/src/main.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
 use clap::Parser;
 use nix_eval::Evaluator;
+use rootcause::{Report, report};
 use serde_json;
 use std::fs;
 use std::io::{self, Read};
@@ -10,19 +10,19 @@ use std::io::{self, Read};
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Nix expression to evaluate (or file path if --file is used)
-    #[arg(value_name = "EXPRESSION")]
+    #[arg(short, long = "expr", value_name = "EXPRESSION")]
     expression: Option<String>,
 
     /// Read expression from file instead of command line
     #[arg(short, long)]
     file: bool,
 
-    /// Output format: nix (default), json
-    #[arg(short, long, default_value = "nix")]
-    format: String,
+    /// Produce output in JSON format, suitable for consumption by another program.
+    #[arg(long)]
+    json: bool,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Report> {
     let args = Args::parse();
 
     // Get the Nix expression to evaluate
@@ -30,7 +30,7 @@ fn main() -> Result<()> {
         // Read from file
         let path = args
             .expression
-            .ok_or_else(|| anyhow::anyhow!("File path required when using --file"))?;
+            .ok_or_else(|| report!("File path required when using --file"))?;
         fs::read_to_string(&path)?
     } else if let Some(expr) = args.expression {
         // Use provided expression
@@ -44,21 +44,13 @@ fn main() -> Result<()> {
 
     // Create evaluator and evaluate
     let evaluator = Evaluator::new();
-    let value = evaluator
-        .evaluate(&expr)
-        .map_err(|e| anyhow::anyhow!("{}", e))?;
+    let value = evaluator.evaluate(&expr).map_err(|e| report!("{}", e))?;
 
     // Output the result
-    match args.format.as_str() {
-        "json" => {
-            println!("{}", serde_json::to_string_pretty(&value)?);
-        }
-        "nix" => {
-            println!("{}", value);
-        }
-        _ => {
-            return Err(anyhow::anyhow!("Unsupported format: {}", args.format));
-        }
+    if args.json {
+        println!("{}", serde_json::to_string_pretty(&value)?);
+    } else {
+        println!("{}", value);
     }
 
     Ok(())
--- rix-parser/.github/ISSUE_TEMPLATE/question.md
+++
@@ -1,7 +0,0 @@
----
-name: Question
-about: Ask a general question about the project
-title: ''
-labels: 'question'
-assignees: ''
----
